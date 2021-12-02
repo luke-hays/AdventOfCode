33c9f5ae0f8cb5b6e7282b5a9ebc9e07630c0558
Midival <lukehays32@gmail.com>
2021-12-01 19:09:59 -0500

    Add first day project

---
+++ 2021/01/main.py
@@ -0,0 +1,18 @@
+# Sub performs a sonar sweeep
+#Each line is a measurement of the sea flor depth
+
+#count the number of times a depth measurement increases
+#from the previous measurement (non for the first)
+
+text = "./sample.txt";
+depthMeasurements = []
+
+with open(text) as file:
+    depthMeasurements = [int(line.strip()) for line in file] 
+
+ctr = 0
+for i in range(1, len(depthMeasurements)):
+    if depthMeasurements[i] > depthMeasurements[i-1]:
+        ctr += 1
+
+print(ctr)
